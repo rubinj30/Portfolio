95de3f3e2f5302bc357a7ce0b4b6b43eb2274c39
Jonathan Rubin <jonathanrubin@Jonathans-MacBook-Pro.local>
2019-01-29 22:10:12 -0500

    Updates

---
+++ precache-manifest.9844a32bb1f11624109b99b7db7df592.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "f64bfd104e7a2c54db80",
+    "url": "/porftolio/static/css/main.59b0412a.chunk.css"
+  },
+  {
+    "revision": "f64bfd104e7a2c54db80",
+    "url": "/porftolio/static/js/main.f64bfd10.chunk.js"
+  },
+  {
+    "revision": "1d4da6f101491bf00609",
+    "url": "/porftolio/static/js/1.1d4da6f1.chunk.js"
+  },
+  {
+    "revision": "4ca2b3b8f4adc4a7dceb",
+    "url": "/porftolio/static/js/runtime~main.4ca2b3b8.js"
+  },
+  {
+    "revision": "f05dc651f4a3f58aeeffa123e2df3bc6",
+    "url": "/porftolio/static/media/connect4.f05dc651.png"
+  },
+  {
+    "revision": "c053a4ba29ef73a2ae80994a8769bdbb",
+    "url": "/porftolio/static/media/JB_card.c053a4ba.jpg"
+  },
+  {
+    "revision": "798fa0acc86ae794d25d67ef67f21d08",
+    "url": "/porftolio/static/media/polaroid2.798fa0ac.png"
+  },
+  {
+    "revision": "c5606e98c4fbdc15e6fde86fb67bca9f",
+    "url": "/porftolio/static/media/JonathanRubin.c5606e98.jpg"
+  },
+  {
+    "revision": "95214bfe33590fe7edb34d5cf04a2044",
+    "url": "/porftolio/index.html"
+  }
+];
